1f233e72b034fcb93d83fef5c35e1d0e95bee51c
rafael martins paiva <rafael.martins.paiva0@gmail.co.>
2024-11-15 17:16:33 -0300

    init update

---
+++ git-pull-chat-app.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+# Caminho do repositório
+REPO_PATH="/data/data/com.termux/files/home/Chat-app"
+
+while true; do
+  # Navega até o diretório do repositório
+  cd "$REPO_PATH" || exit
+
+  # Executa o git pull
+  git pull
+
+  # Espera 10 segundos antes de rodar novamente
+  sleep 10
+done
